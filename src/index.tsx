79ee2b4c06388ada334cd8ec7d38a5369d00c047
chopperqt <ilua292010@gmail.com>
2023-07-31 22:51:38 +0600

    Update `Antd Design`

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import { store } from './services/store';
+import App from "./App";
+import { store } from "./services/store";
 
-import 'antd/dist/antd.compact.css'
+import "antd/dist/reset.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
