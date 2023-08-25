710efb7492934240c9ee9f873a636e64bd64a9df
chopperqt <ilua292010@gmail.com>
2023-08-25 21:00:48 +0600

    Added schema and valibot

--- src/models/library.tsx
+++ src/models/library.tsx
@@ -1,8 +1,12 @@
-export interface LibraryWord {
-  id: number;
-  userID: string;
-  word: string;
-  translate: string[];
-  pined: boolean;
-  createdAt: Date | string;
-}
+import { array, boolean, date, number, object, Output, string } from "valibot";
+
+export const WordSchema = object({
+  id: number(),
+  userID: string(),
+  word: string(),
+  translate: array(string()),
+  pinded: boolean(),
+  createdAt: date(),
+});
+
+export type LibraryWord = Output<typeof WordSchema>;
